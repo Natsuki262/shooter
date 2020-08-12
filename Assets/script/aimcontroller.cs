19c00ae9c1d8c6afad76cecffa4e6eb93aacb970
アレン <77>
2020-08-12 16:30:50 +0900

    カーソルを照準器に変更

---
+++ Assets/script/aimcontroller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aimcontroller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private Texture2D cursor;
+    ///int mouse;
+    void Start()
+    {
+        Cursor.SetCursor(cursor, new Vector2(cursor.width / 2, cursor.height / 2), CursorMode.ForceSoftware);
+        //cursorを自前のテクスチャデータに変更
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         //mouse= Input.mousePosition.x;
+
+    }
+}
