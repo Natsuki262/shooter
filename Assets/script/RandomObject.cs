4b3db42de1cc791ea1b6afad55f5e1cdc55f9bdc
ArenKinatsu <77>
2020-08-22 19:13:02 +0900

    rayの処理を復活させた

--- Assets/script/RandomObject.cs
+++ Assets/script/RandomObject.cs
@@ -10,7 +10,7 @@ public class RandomObject : MonoBehaviour
     public int m_targetCounter;
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
--- Assets/script/aimcontroller.cs
+++ Assets/script/aimcontroller.cs
@@ -7,31 +7,34 @@ public class aimcontroller : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField]
     private Texture2D cursor;
-    ///int mouse;
+    public GameObject target;
+    float mouse;
     void Start()
     {
         Cursor.SetCursor(cursor, new Vector2(cursor.width / 2, cursor.height / 2), CursorMode.ForceSoftware);
         //cursorを自前のテクスチャデータに変更
+        this.target = GameObject.Find("target");
     }
 
     // Update is called once per frame
     void Update()
     {
         //メイン📷上のマウスポインタからRayを飛ばす。
-        //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-        //int layMask = 1;
-        //float maxDistance = 10;
+        int layMask = 1;
+       float maxDistance = 10;
 
-        //RaycastHit2D hit = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction, maxDistance, layMask);
-       /*if(hit.collider)
+        RaycastHit2D hit = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction, maxDistance, layMask);
+       if(hit.collider)
         {
             Debug.Log(hit.collider.gameObject.name);
-        }*/
-        //mouse= Input.mousePosition.x;
-        if(Input.GetMouseButton(0))
+        }
+        mouse= Input.mousePosition.x;
+        if(Input.GetMouseButtonDown(0))
         {
             Debug.Log("hit");
+            Destroy(target);
         }
 
     }
