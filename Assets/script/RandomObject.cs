b771010e74fbc80c265c3bd290d78317aa881a81
ArenKinatsu <77>
2020-08-25 16:40:38 +0900

    targetプレハブの生成をランダム生成をやめ、決まった場所に生成するように変更
    位置はまだ要修正

--- Assets/script/RandomObject.cs
+++ Assets/script/RandomObject.cs
@@ -10,15 +10,23 @@ public class RandomObject : MonoBehaviour
     public int m_targetCounter;
     void Start()
     {
-        
+        TargetInstanse();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float x = Random.Range(Screen.width * 0.1f, Screen.width * 0.9f);
+        /*float x = Random.Range(Screen.width * 0.1f, Screen.width * 0.9f);
         float y = Random.Range(Screen.height * 0.1f, Screen.height * 0.9f);
-        Vector2 screenPoint = new Vector2(x, y);
+        Vector2 screenPoint = new Vector2(x, y);*/
+
+    }
+    void TargetInstanse()
+    {
+        float x = Screen.width / 2;
+        float y = Screen.height / 2;
+
+        Vector2 screenPoint = new Vector2(5, y);
         Vector3 position = Camera.main.ScreenToWorldPoint(screenPoint);
         position.z = 0;
 
@@ -27,12 +35,13 @@ public class RandomObject : MonoBehaviour
         //float x = Random.Range(-10.0f, 10.0f);
         //float y = Random.Range(-5f, 5f);
         //float z = Random.Range(0.0f, 5.0f);
-        if (m_targetCounter < 10)
-        {
 
+        for (int i = 0; i < 5; i++)
+        {
             Instantiate(target, position, Quaternion.identity);
-            m_targetCounter += 1;
+            position = new Vector3(1, 0, 0);
         }
+
 
     }
 }
