fa5052cd88ee791fc0c601f363b05afb1067fee4
ArenKinatsu <77>
2020-08-28 17:10:28 +0900

    等間隔に表示は一応完了骨組みを作成中

--- Assets/script/RandomObject.cs
+++ Assets/script/RandomObject.cs
@@ -8,6 +8,8 @@ public class RandomObject : MonoBehaviour
     // Start is called before the first frame update
     public GameObject target;
     public int m_targetCounter;
+
+    public Vector3[] m_position;//いくつずらすかのメンバー配列
     void Start()
     {
         TargetInstanse();
@@ -26,7 +28,7 @@ public class RandomObject : MonoBehaviour
         float x = Screen.width / 2;
         float y = Screen.height / 2;
 
-        Vector2 screenPoint = new Vector2(5, y);
+        Vector2 screenPoint = new Vector2(1f, y);
         Vector3 position = Camera.main.ScreenToWorldPoint(screenPoint);
         position.z = 0;
 
@@ -36,10 +38,16 @@ public class RandomObject : MonoBehaviour
         //float y = Random.Range(-5f, 5f);
         //float z = Random.Range(0.0f, 5.0f);
 
-        for (int i = 0; i < 5; i++)
+        /*for (int i = 0; i < 5; i++)
         {
-            Instantiate(target, position, Quaternion.identity);
-            position = new Vector3(1, 0, 0);
+            Instantiate(target, position, Quaternion.identity);//実際に出してる場所
+            position += new Vector3(5, 0, 1);//ずつずらす
+
+        }*/
+        for (int i = 0; i < m_position.Length; i++)//配列の長さ分回す
+        {
+            Instantiate(target, m_position[i], Quaternion.identity);
+
         }
 
 
